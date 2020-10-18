686356842f0cd1c93d1309bb19d4493a1851ac50
Arnaud <mrandriamihaja2@gmail.com>
2020-10-18 18:52:43 +0300

    crud categorie fournisseurs

---
+++ src/Form/FournisseursType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Fournisseurs;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FournisseursType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('societe')
+            ->add('contact')
+            ->add('fonction')
+            ->add('addresse')
+            ->add('ville')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Fournisseurs::class,
+        ]);
+    }
+}
