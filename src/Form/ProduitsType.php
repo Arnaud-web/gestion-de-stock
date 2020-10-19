74d7dc0da1b8bef5c013f361bb0dc29e71127a3e
Arnaud <mrandriamihaja2@gmail.com>
2020-10-19 07:39:09 +0300

    relation produit fournisseur categorie

--- src/Form/ProduitsType.php
+++ src/Form/ProduitsType.php
@@ -2,9 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Categories;
 use App\Entity\Produits;
+use App\Entity\Fournisseurs;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProduitsType extends AbstractType
@@ -12,12 +15,19 @@ class ProduitsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('categorie', EntityType::class, [
+                'class'=> Categories::class,
+                'choice_label' => 'nom'
+            ])
             ->add('nom')
             ->add('quantite')
             ->add('prix')
             ->add('description')
-            ->add('fournisseur')
-            ->add('categorie')
+            ->add('fournisseur', EntityType::class, [
+                'class'=> Fournisseurs::class,
+                'choice_label' => 'societe'
+            ])
+            // ->add('categorie')
             ->add('createdAt')
 
         ;
